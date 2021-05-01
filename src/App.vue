0058c5e1c426a8423a9e07f32bf966912e226259
MarcRibera <mribera@thesocialcoin.com>
2021-05-01 02:09:07 +0200

    create members barchart sections and add to main layout

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
           <DateRangePicker v-on:getCurrentDates="manageReceivedDates" />
         </el-header>
         <el-main>
-          <p>Current dates received {{ dates }}</p>
+          <MembersBarchart :data="membersData" />
         </el-main>
         <el-footer>Footer</el-footer>
       </el-container>
@@ -17,13 +17,38 @@
 
 <script>
 import DateRangePicker from "./components/DateRangePicker.vue";
+import MembersBarchart from "./components/MembersBarchart.vue";
 
 export default {
   name: "App",
-  components: { DateRangePicker },
+  components: { DateRangePicker, MembersBarchart },
   data() {
     return {
       dates: [],
+      membersData: [
+        {
+          name: "Meriadock",
+          dates: [
+            "2021-03-1",
+            "2021-03-2",
+            "2021-03-3",
+            "2021-03-4",
+            "2021-03-5",
+          ],
+          values: [5, 6, 2, 3, 5.5],
+        },
+        {
+          name: "Pippin",
+          dates: [
+            "2021-03-1",
+            "2021-03-2",
+            "2021-03-3",
+            "2021-03-4",
+            "2021-03-5",
+          ],
+          values: [3, 5, 8, 1, 3.5],
+        },
+      ],
     };
   },
   methods: {
---
+++ src/components/MembersBarchart.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-row>
+    <el-col :span="12" v-for="(member, index) in data" :key="index">
+      <BarChart
+        :dates="member.dates"
+        :values="member.values"
+        :title="member.name"
+      />
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import BarChart from "./BarChart.vue";
+
+export default {
+  components: { BarChart },
+  props: {
+    data: Array,
+  },
+  data() {
+    return {
+      dates: ["2021-03-1", "2021-03-2", "2021-03-3", "2021-03-4", "2021-03-5"],
+      values: [5, 6, 2, 3, 5.5],
+    };
+  },
+};
+</script>
+
+<style lang="css" scoped></style>
