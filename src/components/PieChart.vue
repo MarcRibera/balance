ce2ae439aa581d5b256f54f67dd20d79682081dd
MarcRibera <mribera@thesocialcoin.com>
2021-08-09 13:21:00 +0200

    add pie chart

---
+++ src/components/PieChart.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="">
+    <highcharts :options="options"></highcharts>
+  </div>
+</template>
+
+<script>
+import { Chart } from "highcharts-vue";
+export default {
+  components: {
+    highcharts: Chart,
+  },
+  props: ["series", "title"],
+
+  computed: {
+    options() {
+      return {
+        chart: {
+          type: "pie",
+        },
+        title: {
+          text: this.title,
+        },
+        series: this.series,
+        plotOptions: {
+          pie: {
+            allowPointSelect: true,
+            cursor: "pointer",
+            dataLabels: {
+              enabled: true,
+              format: "<b>{point.name}</b>: {point.percentage:.1f} %",
+            },
+          },
+        },
+      };
+    },
+  },
+  data() {
+    return {
+      optionsTest: {},
+    };
+  },
+  created() {},
+};
+</script>
