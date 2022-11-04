312bbf75cd4a6d62df6166ff99884485db47da9b
MarcRibera <mribera@thesocialcoin.com>
2022-11-04 16:45:28 +0100

    remove old files

--- public/sprintCodes.js
+++
@@ -1,8 +0,0 @@
-// TODO: automate collect sprint codes
-// export function getSprintsCodes() {
-//   return ['q304', 'q305', 'balances'];
-// }
-
-export function getSprintsCodes() {
-  return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-}
